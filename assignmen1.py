fee2fa7f4171918ecea7dd28917f9c297cd3f49f
uchemcynthia <uchemary098@gmail.com>
2023-03-02 21:26:21 +0100

    Assignment_ver4

--- assignmen1.py
+++ assignmen1.py
@@ -17,14 +17,14 @@ Fact_sal = pd.DataFrame([
 print(Paye)
 print(Fact_sal)
 #add a new row
-print("\nadding new rows using loc\n")
+print("\nadding new rows using loc")
 Paye.loc["Odubuiyi Kehji"]= {
     "Designation":"Front Desk","Basic_sal":100,
     "Tax_payable":12,"Tax_ID":"N-4373889"
     }
 print(Paye)
 #add new rows
-print("\nadding of new rows using concat\n")
+print("\nadding of new rows using concat")
 Paye2=pd.DataFrame({
     "Designation":["Marketing","Production"],"Basic_sal":[500,300],"Tax_payable":[18,35],
     "Tax_ID":["N-4377682","N-4372175"]},index=["Oke Olayemi", "Okoye Chioma"])
@@ -32,7 +32,7 @@ paye=pd.concat([Paye,Paye2])
 print(paye)  
 #loc cannot be used to add multiple rows but concat or Paye.append(Paye2) does a good job
 # add new columns
-print("\n adding using loc and bracket\n")
+print("\n adding using loc and bracket")
 paye.loc[:, ["Dev_levy"]]=[10,10,10,10,10,10,10,10,10,10,10]
 print(paye)
 #Note forthe above to work the len must correspond
@@ -40,30 +40,30 @@ print(paye)
 paye["Nationality"]=["NIG","GHN","NIG","USA","CND","NIG","NGN","SNG","NGN","NIG","IRQ"]
 print(paye)
 #adding new columns using insert
-print("\n inserts column in desired position\n")
+print("\n inserts column in desired position")
 paye.insert(0,"Exits_in_Etax",["Y","N","Y","Y","Y","Y","Y","N","Y","N","Y"])
 print(paye)
 #The above inserts the column in the desired position
-print("\nRename a row\n")
+print("\nRename a row")
 paye.rename(index={"NWAIWU Emeka":"Emeka Hudson"},inplace=True)
 print(paye)
-print("\nRename multiple rows\n")
+print("\nRename multiple rows")
 
 paye.rename(index={"AMINU Abosede":"Aminu Precious","Okoye Chioma":"Obidike Chioma"},
 columns={"Tax_ID":"Tax_id","Basic_sal":"Gross_sal"},inplace=True)
 print(paye)
 
-print("\nChange index position of a column\n")
+print("\nChange index position of a column")
 Pa=paye.set_index(["Tax_id"])
 print(Pa)
-print("\nThe change of the index col is temporary and what happens to the previous index column\n")
+print("The change of the index col is temporary and what happens to the previous index column\n")
 new_order=[2,4,3,1,-1,0,5]
 print(paye[paye.columns[new_order]])
-print("\nThe order of arrangement is temporary too\n")
+print("The order of arrangement is temporary too\n")
 #Delete rows
 paye.drop(index={"AZUBUIKE EBERE","ABAYOMI KUJORE"},inplace=True)
 print(paye)
-print("\nDeleting of rows\n")
+print("\nDeleting of rows")
 #Delete columns
 paye.drop(columns="Dev_levy",inplace=True)
 print(paye)
